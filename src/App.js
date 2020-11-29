8a23b53325711ed7818e2e5d903ce7c32776831a
Arth Patel <arthpatelf50@gmail.com>
2020-11-29 13:14:32 +0530

    updated handler for country picker

--- src/App.js
+++ src/App.js
@@ -15,16 +15,17 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
-  handleCountryChange = async (country) => {
-    console.log(country);
+  handleCountryChange = async (country) => { 
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country }); 
   }
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
         <Cards data = {data}/>
         <CountryPicker handleCountryChange = {this.handleCountryChange}/>
-        <Chart data = {data}/>
+        <Chart data = {data} country={country}/>
       </div>
     )
   }
