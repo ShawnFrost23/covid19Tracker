4fa1650438ef299945b4ad38a3331985aa25f789
Arth Patel <arthpatelf50@gmail.com>
2020-11-29 12:21:51 +0530

    return data from API

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
-    console.log(data);
+    return data;
   } catch (error) {
     
   }
