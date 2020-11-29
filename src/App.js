2387ce33c62a6ecca3c252acdfbdf2626664f508
Arth Patel <arthpatelf50@gmail.com>
2020-11-29 13:01:00 +0530

    added function to handle country change

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ class App extends React.Component {
 
   state = {
     data: {},
+    country: ''
   }
 
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    console.log(country);
+  }
   render() {
     const { data } = this.state;
     return (
       <div className={styles.container}>
         <Cards data = {data}/>
-        <CountryPicker data = {data}/>
+        <CountryPicker handleCountryChange = {this.handleCountryChange}/>
         <Chart data = {data}/>
       </div>
     )
