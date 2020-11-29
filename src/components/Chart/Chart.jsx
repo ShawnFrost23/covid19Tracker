2b7397391d19e960595d0f61c7bd00ae13c681cb
Arth Patel <arthpatelf50@gmail.com>
2020-11-29 12:26:13 +0530

    Added Line chart for gloal data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,18 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import fetchDailyData from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
+
 const Chart = () => {
-  const [dailyDate, setDialyData] = useState({});
+  const [dailyData, setDialyData] = useState([]);
 
   useEffect(() => {
-    const fetchAPI = async => {
-      setDialyData(fetchDailyData());
+    const fetchAPI = async () => {
+      setDialyData(await fetchDailyData());
     }
-
+    console.log(dailyData)
     fetchAPI();
   });
-
+  
+  const lineChart = (
+    dailyData[0]
+      ? (<Line 
+          data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets: [{
+              data: dailyData.map(({ confirmed }) => confirmed),
+              label: 'Infected',
+              borderColor: '#3333ff',
+              fill: true,
+            }, {
+              data: dailyData.map(({ deaths }) => deaths),
+              label: 'Deaths',
+              borderColor: 'red',
+              backgroundColor: 'rgba(255, 0, 0, 0.5)',
+              fill: true,
+            }],
+          }}
+        />) : null
+  )
   return (
-    <h1>Chart</h1>
+    <div className={styles.container}>
+      {lineChart}
+    </div>
   );
 }
 
